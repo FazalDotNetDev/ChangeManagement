055715c7d5871877a411ac99c30247752861cd68
Fazal Abbas <fazalabbas123923@gmail.com>
2023-08-27 21:34:04 +0500

    second

---
+++ src/Application/Contracts/Persistence/Repositories/IUsersRepository.cs
@@ -0,0 +1,15 @@
+﻿using Application.DTOs;
+using Application.Models.Identity;
+using Domain.Entities;
+
+namespace Application.Contracts.Persistence.Repositories
+{
+    public interface IUserRepository
+    {
+        Task<UserDto?> AddUserAsync(UserDto model);
+        Task<Users> GetUserAsync(string userName);
+        Task<List<Users>> GetAllUsersAsync();
+        Task<List<UserProjects>> GetAllUsersProjectsAsync();
+        Task<int> UpdateUsersAsync(UserDto userDto);
+    }
+}
